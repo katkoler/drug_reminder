d0733e52e69a5ab8ba22252852eff6cd9b37322b
Kat Koler <katjusa.koler@gmail.com>
2018-03-11 13:42:50 +0000

    change background

--- timed_reminders/app.py
+++ timed_reminders/app.py
@@ -14,7 +14,7 @@ NEXMO_API_SECRET = os.getenv("NEXMO_API_SECRET")
 
 
 TO_NUMBER = "447543610370"
-reminder = {"Drug_name": "aspirin", "Interval": 1, "Unit":"hours", "Amount":20, "Start date":"2018/03/10"}
+reminder = {"Drug_name": "aspirin", "Interval": 1, "Unit":"hours", "Amount":20, "Start date":"2018/03/10", "Phone_number":123}
 
 
 client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
@@ -26,13 +26,34 @@ def index():
 
 
 # @app.schedule(Rate(1, unit=Rate.MINUTES))
-@app.route('/send')
+@app.route('/send1')
 def handler(foo):
 	resp = client.send_message({
-	    'from': 'reminder',
+	    'from': 'Drug rmndr',
 	    'to': TO_NUMBER,
 	    # 'to': "447805946442",
-	    'text': 'drug reminder!!',
+	    'text': '''Hi Kat, we will reminding you to take Ibuporfen. Here's what you should be careful with:
+      drug interactions: 19-norandrostenedione, Acebutolol, Abciximab, Zorubicin
+      food interactions: Avoid alcohol, Food delays the time to reach peak plasma concentrations by 30-60 minutes and reduces peak plasma concentrations by 30-50%. Extent of absorption is unaffected., Take with food to reduce gastric irritation.
+      toxicity: 
+Side effects: May cause peripheral edema and fluid retention. Use caution in patients with congestive heart failure or severe uncontrolled hypertension. May cause dyspepsia, heartburn, nausea, vomiting, anorexia, diarrhea, constipation, stomatitis, flatulence, bloating, epigastric pain, and abdominal pain. Peptic ulcer and GI bleeding have been reported. May also cause dizziness, headache and nervousness. Acute renal failure accompanied by acute tubular necrosis has been reported.
+
+Most common symptoms of overdose are abdominal pain, nausea, vomiting, lethargy, vertigo, drowsiness (somnolence), dizziness and insomnia. Other symptoms of overdose include headache, loss of consciousness, tinnitus, CNS depression, convulsions and seizures. May rarely cause metabolic acidosis, abnormal hepatic function, hyperkalemia, renal failure, dyspnea, respiratory depression, coma, acute renal failure, and apnea (primarily in very young pediatric patients).
+
+LD50=1255mg/kg(orally in mice)
+
+    We are aware this is very technical. If you have any questions, please consult your doctor.''',
+	})
+	print(resp)
+	return 'ok'
+
+@app.route('/send2')
+def handler(foo):
+	resp = client.send_message({
+	    'from': 'Drug rmndr',
+	    'to': TO_NUMBER,
+	    # 'to': "447805946442",
+	    'text': 'Hi Kat, please make sure you take your scheduled Ibuprofen',
 	})
 	print(resp)
 	return 'ok'
