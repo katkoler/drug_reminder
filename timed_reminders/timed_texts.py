8bc7770eedff98bce4b69217423080364ff7d1e1
Kat Koler <katjusa.koler@gmail.com>
2018-03-11 14:30:55 +0000

    move titles

--- timed_reminders/timed_texts.py
+++ timed_reminders/timed_texts.py
@@ -3,16 +3,16 @@ import os
 import nexmo
 import config #need your config file 
 
-NEXMO_API_KEY = os.environ["e68cada7"]
-NEXMO_API_SECRET = os.environ["AXb198qZSVjABNPp"]
+NEXMO_API_KEY = "e68cada7"
+NEXMO_API_SECRET = "AXb198qZSVjABNPp"
 
 client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
 TO_NUMBER = "447543610370"
 
 
 client.send_message({
-    'from': 'drug reminder service',
-    'to': TO_NUMBER,
-    # 'to': "447805946442",
-    'text': 'drug reminder!!',
-})
+	    'from': 'Drug rmndr',
+	    'to': TO_NUMBER,
+	    # 'to': "447805946442",
+	    'text': 'Hi Kat, please make sure you take your scheduled Ibuprofen',
+	})
